886b16ec85bd84eee34e413636d3f75c649e8771
Abraham Owodunni <Owodunniabraham@gmail.com>
2022-09-21 22:52:21 +0000

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -322,6 +322,7 @@ if download:
 
         return df
     df = pd.read_csv(f"stocks {selected_stock}.csv", parse_dates = ['Date'])
+    st.write("trying out the head")
     st.write(df.head())
     st.write(df.info())
     last_date= str(df.Date.dt.date[0])
